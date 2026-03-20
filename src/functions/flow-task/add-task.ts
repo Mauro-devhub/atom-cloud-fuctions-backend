86445a291d4b9fb98912bcac5dae5b9a7daba198
mauricio <dsmauricio16@gmail.com>
2026-03-20 08:15:31 -0400

    pipe added to deploy functions

--- src/functions/flow-task/add-task.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from "@google-cloud/functions-framework";
-import { Request, Response } from "express";
-
-export const addTaskHandler = (req: Request, res: Response) => {
-    res.send("Add Task")
-}
-
-functions.http("addTask", addTaskHandler);
